f2e2a9d91cb59500d17f1fad647ff21b63a569d1
Hyesoo <69658240+Hyesoo-J@users.noreply.github.com>
2022-05-16 15:58:09 +0200

    Update User.java

--- app/src/main/java/com/se/User.java
+++ app/src/main/java/com/se/User.java
@@ -4,14 +4,16 @@ public class User {
 
     public String email;
     public String name;
+    //public String number;
 
     public User(String email, String name) {
         this.email = email;
         this.name = name;
+        //this.number = number;
     }
 
     @Override
     public String toString() {
-        return email + ',' + name;
+        return email + ',' + name; // + ',' + number;
     }
 }
